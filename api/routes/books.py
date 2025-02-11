cb3fda45e134b356e022bbb75de287fba851744e
Ettu Julius Yonwatode <juliusyonwatade@gmail.com>
2025-02-11 14:30:33 +0100

    implement the get input endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,13 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/books/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    if book_id not in db.books:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Book not found"},
+        )
+    return db.get_book(book_id)
