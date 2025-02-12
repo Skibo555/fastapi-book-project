9fb63da7a97b83c94180be2c085353ef316967ca
Julius Yonwatode <juliusyonwatade@gmail.com>
2025-02-12 03:16:40 +0000

    add a workflow

--- api/routes/books.py
+++ api/routes/books.py
@@ -62,7 +62,7 @@ async def delete_book(book_id: int) -> None:
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
 
-@router.get("/books/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book(book_id: int) -> Book:
     if book_id not in db.books:
         return JSONResponse(
